e9bdbe45fe752250ba2a8f0dd9d964119db85b5b
mmoussel <mustafa@finflx.com>
2023-07-13 11:56:33 +0300

    feat: add svgs icons

---
+++ src/assets/svgs/index.ts
@@ -0,0 +1,4 @@
+export { default as BackIcon } from './back.svg';
+export { default as ArrowRightIcon } from './arrow-right.svg';
+export { default as StockIcon } from './stock.svg';
+export { default as VendorsIcon } from './vendors.svg';
